676b4ca617505cb4ba7d14f835d311e08e44f01a
Mehemmed09 <tu7pj2v4h@code.edu.az>
2024-04-17 13:17:53 +0400

    added

---
+++ index.js
@@ -0,0 +1,8 @@
+function addItem(inputId) {
+    var inputValue = document.getElementById(inputId).value;
+    if (inputValue !== '') {
+     alert('cavab:' + inputValue);
+    } else {
+      alert('sozden zaddan yaZdanaaa!');
+    }
+  }
